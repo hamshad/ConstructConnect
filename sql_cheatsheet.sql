29942c6319cc4219136c285ff95bd96b96cca64b
hamshad <hiromega18@gmail.com>
2025-03-17 02:42:50 +0530

    chore: new column

--- sql_cheatsheet.sql
+++ sql_cheatsheet.sql
@@ -13,8 +13,16 @@ WHERE company_id IN (
     HAVING COUNT(*) > 1
 );
 
+-- Add new columns to store additional JSON data
+ALTER TABLE IF EXISTS public.companies
+    ADD COLUMN IF NOT EXISTS source_company_id integer,
+    ADD COLUMN IF NOT EXISTS last_updated_date timestamp,
+    ADD COLUMN IF NOT EXISTS associated_contacts jsonb,
+    ADD COLUMN IF NOT EXISTS company_portfolio jsonb,
+    ADD COLUMN IF NOT EXISTS company_notes jsonb;
+
 -- [Search from the string]
--- SELECT * FROM companies WHERE address::text ilike '%Santa Monica%';
+SELECT * FROM companies WHERE address::text ilike '%Santa Monica%';
 
 -- [Search from the jsonb]
 select * from companies where address->>'city' = 'Santa Monica';
