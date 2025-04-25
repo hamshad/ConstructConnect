712cf8e02e86cd84f4cf55abfa2f674146187cb6
hamshad <hiromega18@gmail.com>
2025-04-25 19:02:02 +0530

    feat: company info done

---
+++ src/companyInfo/companyInfo.ts
@@ -0,0 +1,177 @@
+import { join } from 'path';
+import { appendToFile } from '../../data/FileOperations';
+import { spinner } from '@clack/prompts';
+import { CompanyInfoSql } from './companyInfoSql';
+
+const DB = new CompanyInfoSql();
+
+const companyInfoPathFile = join(process.cwd(), 'src', 'companyInfo', 'companyInfoPaths.json');
+
+async function fetchCompanyInfo(companyId: string): Promise<SingleCompanyType | undefined> {
+  let url;
+  try {
+    url = new URL('https://api.io.constructconnect.com/cc/v1/companyInfo');
+  } catch (error) {
+    throw new Error('Invalid URL');
+  }
+
+  if (Bun.env.API_PUBLIC_KEY === undefined) {
+    throw new Error('API_PUBLIC_KEY is required');
+  }
+
+  url.searchParams.set('x-api-key', Bun.env.API_PUBLIC_KEY);
+  url.searchParams.set('companyId', companyId);
+
+  const response = await fetch(url.toString(), {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    console.error('Error fetching company info:', JSON.stringify(response, null, 2));
+    console.error(`HTTP error! status: ${response.status}`);
+    const curatedProjectApiError = join(process.cwd(), 'src', 'companyInfo', 'CompanyInfoApiError.json');
+    await Bun.write(curatedProjectApiError, `Error fetching company info no. ${companyId} with status code ${response.status}: ${JSON.stringify(response, null, 2)}`);
+    return undefined;
+  }
+
+  return response.json();
+}
+
+async function addAllCompanyInfoToPostgresqlFromApi(data: SingleCompanyType[]): Promise<void> {
+  for (const companyInfo of data) {
+    console.log(companyInfo)
+
+    await DB.insert(companyInfo);
+  }
+
+  console.log("\n");
+  console.log("Company Info added/updated successfully!");
+  console.log("\n");
+  console.log("\n");
+}
+
+export async function countCuratedProjectsInFile(): Promise<number> {
+  try {
+
+    let numberOfCompanyInfo: number = 0;
+
+    if (!await Bun.file(companyInfoPathFile).exists()) {
+      console.log(`${companyInfoPathFile} does not exist`);
+      return 0;
+    }
+
+    const projectPaths = await Bun.file(companyInfoPathFile).json();
+
+    for (const filePath of projectPaths) {
+
+      if (!await Bun.file(filePath).exists()) {
+        console.log(`${filePath} does not exist`);
+        return 0;
+      }
+
+      const fileContent = await Bun.file(filePath).text();
+      const data: [any] = JSON.parse(fileContent);
+
+      numberOfCompanyInfo += data.length;
+    }
+
+    return numberOfCompanyInfo;
+
+  } catch (error) {
+    console.error('Error reading file:', error);
+    return 0;
+  }
+}
+
+/**
+* Adds all company info to PostgreSQL database from the local cached data JSON file.
+*
+* @param {string} filePath - The path to the JSON file containing company info data
+*/
+export async function addAllCompanyInfoToPostgresqlFromFile(): Promise<void> {
+
+  if (!await Bun.file(companyInfoPathFile).exists()) {
+    console.log(`${companyInfoPathFile} does not exist`);
+    return;
+  }
+
+  const companyPaths = await Bun.file(companyInfoPathFile).json();
+
+  for (const filePath of companyPaths) {
+    console.log('Project Path: ', filePath);
+
+    if (!await Bun.file(filePath).exists()) {
+      console.log(`${filePath} does not exist`);
+      continue;
+    }
+
+    const s = spinner();
+    s.start('Adding Company Info to database...');
+
+    const fileContent = await Bun.file(filePath).arrayBuffer();
+    const data: SingleCompanyType[] = JSON.parse(new TextDecoder().decode(fileContent));
+
+    let i = 0;
+    for (const company of data) {
+
+      await DB.insert(company);
+
+      i++;
+      s.message(`Adding Company Info to database... ${i}/${data.length}`);
+    }
+
+    console.log("\n");
+    s.stop('Company Info added/updated successfully! Total Company Info added: ' + data.length);
+    console.log("\n");
+    console.log("\n");
+  }
+}
+
+
+/**
+* Fetches all Company Info from the API and appends them to a JSON file.
+*
+* @param {number} existingRecords - The number of existing records in the JSON file (optional)
+* @returns {Promise<void>}
+*/
+export async function getAllCompanyInfo(existingRecords?: number): Promise<void> {
+  const LIMIT = 50;
+  let offset: number = Number(existingRecords) ?? 0;
+  const totalRecords: number = await DB.getAll();
+
+  while (offset < totalRecords) {
+    console.log('Offset/Total: ', offset, '/', totalRecords);
+
+    // Creating a file and push it on the array
+    const outputFilePath: string = join(process.cwd(), 'data', `comapany_info_${offset}-${offset + LIMIT}.json`);
+
+    await appendToFile<string>(companyInfoPathFile, outputFilePath);
+
+
+    console.log(`Offset: ${offset}`);
+    // NOTE: unknown type is used here to avoid type errors
+    // and overlap another type on the default type
+    const getCompanyIds: unknown = await DB.getAllIds(LIMIT, offset);
+    const companyIds = (getCompanyIds as { project_id: string }[]).map((project) => project.project_id);
+
+    for (const companyId of companyIds) {
+
+      console.log(`Fetching company id with ID: ${JSON.stringify(companyId)}`);
+
+      const response = await fetchCompanyInfo(companyId);
+
+      // Check if the response is valid
+      if (response) {
+        await appendToFile<SingleCompanyType>(outputFilePath, response);
+        console.log(`Fetched ${response.companyInformation[0].Id} company info successfully`);
+      }
+    }
+
+    offset += Math.abs(offset - totalRecords) < 100 ? offset - totalRecords : LIMIT;
+  }
+
+  console.info('All curated projects fetched successfully');
+}
---
+++ src/companyInfo/companyInfoSql.ts
@@ -0,0 +1,84 @@
+import { SQL } from '../utils/SQL';
+import type { TableSql } from '../utils/TableSql';
+
+export class CompanyInfoSql implements TableSql<SingleCompanyType> {
+
+  title = 'Company Info';
+  table_name = 'company_details';
+
+  async getLength() {
+    const result = await SQL.client.query(`SELECT COUNT(*) FROM ${this.table_name};`,);
+    console.log('Project length:', result.rows[0].count);
+    return result.rows[0].count;
+  }
+
+  async getAll(limit?: number) {
+    console.log('start transaction');
+    try {
+      console.log('Getting all companies...');
+      const result = await SQL.client.query(
+        'SELECT * FROM company_leads LIMIT $1;',
+        [limit ?? 10000]
+      );
+      console.log('commit transaction');
+      return result.rows[0].count;
+    } catch (error) {
+      console.error('Error fetching companies:', error);
+      throw error;
+    }
+  }
+
+  async getAllIds(limit?: number, offset?: number): Promise<string[]> {
+    try {
+      console.log('Getting all companies id...');
+      const result = await SQL.client.query('SELECT company_id FROM companies LIMIT $1 OFFSET $2;', [limit ?? 10000, offset ?? 0]);
+      return result.rows;
+    } catch (error) {
+      console.error('Error fetching companies:', error);
+      throw error;
+    }
+  }
+
+  async insert(data: SingleCompanyType): Promise<void> {
+
+    try {
+      // Use the first company information record
+      const companyInfo = data.companyInformation[0];
+
+      const query = `
+      INSERT INTO public.company_details (
+        id,
+        company_id,
+        source_company_id,
+        website,
+        fax,
+        email_address,
+        contacts,
+        portfolio,
+        notes
+      )
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+      ON CONFLICT (id) DO NOTHING
+      RETURNING *;
+    `;
+
+      const values = [
+        companyInfo.Id,
+        companyInfo.SourceCompanyId.toString(),
+        companyInfo.SourceCompanyId.toString(),
+        companyInfo.Website || null,
+        companyInfo.Fax ? companyInfo.Fax.toString() : null,
+        companyInfo.EmailAddress || null,
+        JSON.stringify(data.associatedContacts || []),
+        JSON.stringify(data.companyPortfolio || []),
+        JSON.stringify(data.companyNotes || [])
+      ];
+
+      const result = await SQL.client.query(query, values);
+      console.log('Inserted company info successfully', result);
+    } catch (err) {
+      console.error('Error inserting company info: ', err);
+      console.info(JSON.stringify(data, null, 2))
+    }
+  }
+}
